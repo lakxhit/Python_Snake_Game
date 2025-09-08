294818ccbb9da30669a31002770989b114e9a08f
lakxhit <159778434+lakxhit@users.noreply.github.com>
2025-09-08 19:01:01 +0530

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,138 @@
+import turtle
+import random
+
+w = 500
+h = 500
+food_size = 10
+delay = 100
+
+offsets = {
+    "up": (0, 20),
+    "down": (0, -20),
+    "left": (-20, 0),
+    "right": (20, 0)
+}
+
+def reset():
+    global snake, snake_dir, food_position, pen, score
+    snake = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
+    snake_dir = "up"
+    food_position = get_random_food_position()
+    food.goto(food_position)
+    move_snake()
+    score = -1
+    update_score()
+
+def move_snake():
+    global snake_dir
+
+    new_head = snake[-1].copy()
+    new_head[0] = snake[-1][0] + offsets[snake_dir][0]
+    new_head[1] = snake[-1][1] + offsets[snake_dir][1]
+
+    if new_head in snake[:-1]:
+        reset()
+    else:
+        snake.append(new_head)
+
+        if not food_collision():
+            snake.pop(0)
+
+        if snake[-1][0] > w / 2:
+            snake[-1][0] -= w
+        elif snake[-1][0] < - w / 2:
+            snake[-1][0] += w
+        elif snake[-1][1] > h / 2:
+            snake[-1][1] -= h
+        elif snake[-1][1] < -h / 2:
+            snake[-1][1] += h
+
+        pen.clearstamps()
+
+        for segment in snake:
+            pen.goto(segment[0], segment[1])
+            pen.stamp()
+
+        screen.update()
+
+        turtle.ontimer(move_snake, delay)
+
+def food_collision():
+    global food_position
+    if get_distance(snake[-1], food_position) < 20:
+        food_position = get_random_food_position()
+        food.goto(food_position)
+        update_score()
+        return True
+    return False
+
+def get_random_food_position():
+    x = random.randint(int(-w / 2 + food_size), int(w / 2 - food_size))
+    y = random.randint(int(-h / 2 + food_size), int(h / 2 - food_size))
+    return (x, y)
+
+def get_distance(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
+    return distance
+
+def go_up():
+    global snake_dir
+    if snake_dir != "down":
+        snake_dir = "up"
+
+def go_right():
+    global snake_dir
+    if snake_dir != "left":
+        snake_dir = "right"
+
+def go_down():
+    global snake_dir
+    if snake_dir != "up":
+        snake_dir = "down"
+
+def go_left():
+    global snake_dir
+    if snake_dir != "right":
+        snake_dir = "left"
+
+def update_score():
+    global score
+    score += 1
+    score_display.clear()
+    score_display.write("Score: " + str(score), align="center", font=("Courier", 24, "normal"))
+
+screen = turtle.Screen()
+screen.setup(w, h)
+screen.title("Snake")
+screen.bgcolor("blue")
+screen.tracer(0)
+
+pen = turtle.Turtle("square")
+pen.penup()
+
+food = turtle.Turtle()
+food.shape("square")
+food.color("yellow")
+food.shapesize(food_size / 20)
+food.penup()
+
+score = 0
+
+score_display = turtle.Turtle()
+score_display.speed(0)
+score_display.color("white")
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, h // 2 - 40)
+update_score()
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_right, "Right")
+screen.onkey(go_down, "Down")
+screen.onkey(go_left, "Left")
+
+reset()
+turtle.done()
